629874627bd8b6a2c4e8a69f4a3dd1ad9ebf2f32
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2023-12-12 12:29:46 +0100

    Change server

--- mqtt_subscriber/lib/main.dart
+++ mqtt_subscriber/lib/main.dart
@@ -34,7 +34,7 @@ class MyHomePage extends StatelessWidget {
         ),
         body: BlocProvider(
           create: (context) => MqttBloc(
-            server: '192.168.1.117',
+            server: '10.0.2.2',
             clientIdentifier: 'flutter_subscriber',
             port: 1883,
           ),
@@ -51,33 +51,32 @@ class MessageWidget extends StatelessWidget {
     final bloc = BlocProvider.of<MqttBloc>(context);
     return BlocBuilder<MqttBloc, AppState>(
       builder: (context, state) => Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text('${state.status}'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Activate'),
-                      Switch(
-                          value: state.activated,
-                          onChanged: (activate) =>
-                              bloc.add(Activate(activate))),
-                    ],
-                  ),
-                ],
-              ),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text('${state.status}'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Activate'),
+                    Switch(
+                        value: state.activated,
+                        onChanged: (activate) => bloc.add(Activate(activate))),
+                  ],
+                ),
+              ],
             ),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: _buildMessageTile,
-                itemCount: bloc.state.messages.length,
-              ),
-            )
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: _buildMessageTile,
+              itemCount: bloc.state.messages.length,
+            ),
+          )
+        ],
+      ),
     );
   }
 
